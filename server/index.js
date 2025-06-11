c9436f56445555f6c41ebc8e8b7e318c857e2c63
Kaung Min Khant <kmkhant1500@gmail.com>
2025-06-11 14:26:02 +0630

    Feat : add regular socket

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const httpServer = createServer();
 
 const io = new Server(httpServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 });
 
@@ -15,4 +15,6 @@ io.on('connection', socket => {
         console.log(data);
         io.emit('message',` ${socket.id.substring(0,5)}: ${data}`)
     })
-})
+});
+
+httpServer.listen(3500, () => console.log('listening on port 3500'))
