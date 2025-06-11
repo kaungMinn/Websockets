4cf1856a753a1bd876a2ffc53581e7de6b02c66c
Kaung Min Khant <kmkhant1500@gmail.com>
2025-06-11 20:12:04 +0630

    Feat : add activity

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3500;
+const ADMIN = "Admin";
 
 const app = express();
 
@@ -16,6 +17,14 @@ const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
 
+// state
+const UsersState = {
+    users: [],
+    setUsers: function(newUsersArray){
+        this.users = newUsersArray
+    }
+}
+
 const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
@@ -23,10 +32,49 @@ const io = new Server(expressServer, {
 });
 
 io.on('connection', socket => {
-    console.log(`User ${socket.id} connected`)
+    console.log(`User ${socket.id} connected`);
+
+    //Upon connection - only to user
+    socket.emit('message', 'Welcome to Chat App!');
+
+    //Upon connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}`)
+
+    //Listening for a message event
     socket.on('message', data => {
         console.log(data);
-        io.emit('message',` ${socket.id.substring(0,5)}: ${data}`)
+        io.emit('message',` ${socket.id.substring(0,5)}: ${data.test}`)
+    })
+
+    //When user disconnect - to all others
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}`)
+    });
+
+    //Listen for activity
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 });
 
+function buildMsg(name, text){
+    return {
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }).format(new Date())
+    }
+}
+
+//User function
+function activateUser(id, name, room){
+    const user = {id, name, room};
+    UsersState.setUsers([
+        ...UsersState.users.filter(user => user.id !== ud),
+        user
+    ])
+    return user;
+}
